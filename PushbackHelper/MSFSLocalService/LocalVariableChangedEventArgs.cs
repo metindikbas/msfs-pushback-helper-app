966f7b7390acb86c8a545c359b587c9ec6a58b13
Björn Hund <bjoern@hund-edv.de>
2021-07-02 16:53:02 +0200

    Add new event arguments to publish the state of local variables

---
+++ PushbackHelper/MSFSLocalService/LocalVariableChangedEventArgs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PushbackHelper.MSFSLocalService
+{
+    public class LocalVariableChangedEventArgs : EventArgs
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public double Value { get; set; }
+    }
+}
---
+++ PushbackHelper/MSFSLocalService/ParkingBrakeStateChangedEventArgs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PushbackHelper.MSFSLocalService
+{
+    public class ParkingBrakeStateChangedEventArgs : EventArgs
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public bool Active { get; set; }
+    }
+}
