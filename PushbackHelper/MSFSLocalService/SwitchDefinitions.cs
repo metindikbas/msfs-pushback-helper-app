ff0d050a83b5f214493bd9ef13dff130c379911a
Björn Hund <bjoern@hund-edv.de>
2021-07-02 16:52:23 +0200

    Add definitions and mapping for local variables

---
+++ PushbackHelper/MSFSLocalService/SwitchDefinitions.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace PushbackHelper.MSFSLocalService
+{
+    public static class SwitchDefinitions
+    {
+        // Assignment lvar to switch types
+        private static readonly Dictionary<string, SwitchType> _switchDefinitions = new Dictionary<string, SwitchType> {
+            { "A32NX_PARK_BRAKE_LEVER_POS", SwitchType.ParkingBrake }
+        };
+
+        public static bool Exists(string name)
+        {
+            return _switchDefinitions.ContainsKey(name);
+        }
+
+        public static SwitchType GetSwitchType(string name)
+        {
+            if (!Exists(name))
+            {
+                return SwitchType.Undefined;
+            }
+
+            return _switchDefinitions[name];
+        }
+    }
+}
